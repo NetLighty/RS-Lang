b5ffa3ecdd937d744f1f82ab54b4b60c82f5e283
irina <irisha.dieriecha@mail.ru>
2022-08-17 22:36:48 +0300

    feat: create authorization form

---
+++ src/components/authForm/authForm.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import './AuthForm.scss';
+import enter from '../../assets/img/enter.svg';
+
+// eslint-disable-next-line react/function-component-definition
+const AuthForm: FC = () => (
+  <div className="form">
+    <img className="form__img" src={enter} alt="planet" />
+    <p className="form__text">Добро пожаловать в RSLang!</p>
+    <input className="form__email" type="email" placeholder="введите email" />
+    <input className="form__password" type="password" placeholder="введите пароль" />
+    <div className="form__button">
+      <button className="form__button-reg" type="submit">регистрация</button>
+      <button className="form__button-enter" type="submit">вход</button>
+    </div>
+  </div>
+);
+
+export default AuthForm;
