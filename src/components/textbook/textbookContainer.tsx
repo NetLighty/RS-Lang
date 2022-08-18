57bb97f5495edd0191cd41987824ecdb0000e70f
Maria22 <m.ushakova22@gmail.com>
2022-08-18 12:42:47 +0200

    feat: add downloading words on page

--- src/components/textbook/textbookContainer.tsx
+++ src/components/textbook/textbookContainer.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from './../../store/index.reducers';
-// import { useDispatch} from 'react-redux';
+import { useDispatch} from 'react-redux';
 import useGetWords from '../../hooks/useGetWords';
 import { IWord } from './../../models/IWord';
+import { addCurrentBookWords } from './../../store/textbook.actions';
 
 function TextbookContainer ():JSX.Element {
-  const { currentWords, getWords, isLoading } = useGetWords();
-  // const dispatch = useDispatch();
-  const [group, setGroup] = useState(0);
-  const [page, setPage] = useState(0);
+  const { bookPageWords, getWords, isLoading } = useGetWords();
+  const dispatch = useDispatch();
+  const [group] = useState(0);
+  const [page] = useState(0);
   const wordsToRender = useSelector((state:RootState)=> state.textbook.bookWords);
 
   useEffect(() => {
     getWords(group, page);
   }, [group,page]);
 
+  useEffect(() => {
+    if(bookPageWords?.length) dispatch(addCurrentBookWords([...bookPageWords]))
+  }, [bookPageWords]);
 
   return (
     <>
+      {isLoading && console.log('loading')}
       {wordsToRender?.length ? wordsToRender.map((word:IWord) => console.log(word)) : null}
     </>
   );
--- src/hooks/useGetWords.ts
+++ src/hooks/useGetWords.ts
@@ -1,24 +1,37 @@
 import { IWord } from './../models/IWord';
 import { RootState } from './../store/index.reducers';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getWordsFromServer } from '../store/word.actions';
-
+ type Request = {
+  group: number| null,
+  page: number| null,
+ }
 export default function useGetWords() {
   const dispatch = useDispatch();
   const words = useSelector((state:RootState)=> state.words);
-  const [currentWords, setCurrentWords] = useState<Array<IWord>|null>(null);
+  const [bookPageWords, setBookPageWords] = useState<Array<IWord>|null>(null);
+  const [request, setRequest] = useState<Request> ({group:null, page:null})
   const [isLoading, setIsLoading] = useState(false);
 
   const getWords = (group:number, page:number) => {
-    if (words[group][page]) {
-      setCurrentWords(words[group][page]);
+    if (Object.keys(words).includes(group.toString()) && words[group][page]) {
+      setBookPageWords(words[group][page]);
       setIsLoading(false);
       return words[group][page];
     } else {
     setIsLoading(true);
+    setRequest({group,page})
     return dispatch(getWordsFromServer(group, page));
     }
   };
-  return { currentWords, getWords, isLoading };
+
+  useEffect(()=>{
+    if(request.group !== null && request.page !== null && Object.keys(words).includes(request.group.toString()) && words[request.group][request.page]) {
+      setBookPageWords(words [request.group][request.page]);
+      setRequest({group:null, page:null});
+    }
+  },[words,request])
+  
+  return { bookPageWords, getWords, isLoading };
 }
