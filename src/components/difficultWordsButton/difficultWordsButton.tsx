0ccf69e159eef8a58f7cb3e5c80f5fc8c2f272ec
NetLighty <83058872+NetLighty@users.noreply.github.com>
2022-08-28 16:03:44 +0300

    refactor: revert diffiiculty words commit

--- src/components/difficultWordsButton/difficultWordsButton.tsx
+++ src/components/difficultWordsButton/difficultWordsButton.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react';
+import React from 'react';
 
 const result = window.location.origin;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 const DifficultWordButton:FC<{ onClick: React.MouseEventHandler<HTMLElement> }> = ({ onClick }) => (
   <button type="button" className="book__difficult__button" onClick={onClick}>
@@ -11,7 +12,21 @@ const DifficultWordsButton = () => (
 >>>>>>> parent of 002cbba (refactor: rename index.reducers.ts)
     <img src={`${result}/src/assets/img/saturn.svg`} alt="Difficult words" className="book__difficult__button__image" />
     <img src={`${result}/src/assets/img/difficult.svg`} alt="Difficult words" className="book__difficult__button__text" />
+=======
+const DifficultWordsButton = () => (
+  <button type="button" className="book__difficult__button">
+    <img
+      src={`${result}/src/assets/img/saturn.svg`}
+      alt="Difficult words"
+      className="book__difficult__button__image"
+    />
+    <img
+      src={`${result}/src/assets/img/difficult.svg`}
+      alt="Difficult words"
+      className="book__difficult__button__text"
+    />
+>>>>>>> parent of 9313ac7 (feat: rewrite difficultWordsbutton)
   </button>
 );
 
-export default DifficultWordButton;
+export default DifficultWordsButton;
