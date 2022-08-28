9313ac756114ed90121ad95a4b563bf25a403f8b
NetLighty <83058872+NetLighty@users.noreply.github.com>
2022-08-28 15:38:56 +0300

    feat: rewrite difficultWordsbutton

--- src/components/difficultWordsButton/difficultWordsButton.tsx
+++ src/components/difficultWordsButton/difficultWordsButton.tsx
@@ -1,20 +1,12 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 const result = window.location.origin;
 
-const DifficultWordsButton = () => (
-  <button type="button" className="book__difficult__button">
-    <img
-      src={`${result}/src/assets/img/saturn.svg`}
-      alt="Difficult words"
-      className="book__difficult__button__image"
-    />
-    <img
-      src={`${result}/src/assets/img/difficult.svg`}
-      alt="Difficult words"
-      className="book__difficult__button__text"
-    />
+const DifficultWordButton:FC<{ onClick: React.MouseEventHandler<HTMLElement> }> = ({ onClick }) => (
+  <button type="button" className="book__difficult__button" onClick={onClick}>
+    <img src={`${result}/src/assets/img/saturn.svg`} alt="Difficult words" className="book__difficult__button__image" />
+    <img src={`${result}/src/assets/img/difficult.svg`} alt="Difficult words" className="book__difficult__button__text" />
   </button>
 );
 
-export default DifficultWordsButton;
+export default DifficultWordButton;
